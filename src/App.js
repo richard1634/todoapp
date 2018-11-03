0efb6146148340e7075bc3ca40eb6f1d279baa71
Will Cheng <willchengsters@gmail.com>
2018-11-02 19:49:34 -0700

    basic todo app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	constructor(props){
+		super(props);
+		this.state={
+			todos: [],
+			input: ''
+		}
+	}
+
+	handleSubmit = (event) => {
+		event.preventDefault()
+		this.setState({
+			todos: [...this.state.todos, this.state.input],
+			input: ''
+		})
+	}
+
+	handleChange = (event) => {
+		this.setState({
+			input: event.target.value
+		})
+	}
+
+
+	render() {
+		console.log(this.state)
+
+		const todo_map = this.state.todos.map((item, index) => {
+			return(
+				<div key={index}>
+					<p> <span>{index}:</span> {item}</p>
+				</div>
+			)
+		})
+
+		return (
+			<div>
+				<form onSubmit={this.handleSubmit} >
+					<label htmlFor="title">Todo:</label>
+					<input onChange={this.handleChange} value={this.state.input} id="title" type="text"/>
+
+					<div>
+						<h3>My to-do list:</h3>
+						{todo_map}
+					</div>
+				</form>
+			</div>
+		);
+	}
 }
 
 export default App;
