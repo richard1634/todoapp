90897fb91807b4e4af477bf75794b5569d400e4f
richard le <rle5@uci.edu>
2018-11-04 19:46:20 -0800

    simple to do app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
-import TodoItem from './todo-item'
-
 import './style.css'
 
 class App extends Component {
 	constructor(props){
 		super(props);
 		this.state={
-			todos: [
-				{name: 'item2', rename_input: ''},
-				{name: 'item1', rename_input: ''}
-			],
+			todos: [],
 			input: ''
 		}
 	}
@@ -29,52 +24,15 @@ class App extends Component {
 		})
 	}
 
-	renameInput_change = (event, item) => {
-		const {todos} = this.state
-		console.log(item)
-
-		for(let i = 0; i < todos.length; i++){
-			if(todos[i] === item){
-				todos[i].rename_input = event.target.value
-			}
-		}
-
-		this.setState({
-			todos
-		})
-
-	}
-
-	renameInput_submit = (event, item) => {
-		event.preventDefault()
-		const {todos} = this.state
-
-		for(let i = 0; i < todos.length; i++){
-			if(todos[i] === item){
-				todos[i].name = todos[i].rename_input
-				todos[i].rename_input = ''
-			}
-		}
-
-		this.setState({
-			todos
-		})
-	}
-
 
 	render() {
 		console.log(this.state)
 
 		const todo_map = this.state.todos.map((item, index) => {
-
 			return(
-				<TodoItem
-					key={index}
-					item={item}
-					index={index}
-					inputHandler = {this.renameInput_change}
-					inputSubmit = {this.renameInput_submit}
-					/>
+				<div key={index} className="todo-item" >
+					<p> <span>{index}:</span> {item}</p>
+				</div>
 			)
 		})
 
